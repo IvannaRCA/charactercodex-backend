d127dfc81316dade43c6686fcf5d183d9a1b0d7c
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-07-21 11:22:32 +0200

    initial commit

---
+++ src/main/java/com/ivanna/charactercodex/CharactercodexApplication.java
@@ -0,0 +1,13 @@
+package com.ivanna.charactercodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharactercodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharactercodexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanna/charactercodex/CharactercodexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanna.charactercodex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactercodexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
