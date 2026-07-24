491824ccd623a948b84ca82f4f84f7e582301727
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-07-24 13:21:14 +0200

    feat: implement Cors config

--- src/main/java/com/ivanna/charactercodex/security/SpringConfig.java
+++ src/main/java/com/ivanna/charactercodex/security/SpringConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.ivanna.charactercodex.security.filter.JWTAuthentication;
 import com.ivanna.charactercodex.security.filter.JWTAuthorization;
@@ -15,12 +16,14 @@ import com.ivanna.charactercodex.security.filter.JWTAuthorization;
 public class SpringConfig {
 
     private final CustomAuthenticationManager customAuthenticationManager;
-    
+    private final CorsConfigurationSource corsConfigurationSource;
+
     @Value("${JWT_SECRET}")
     private String secret;
 
-    public SpringConfig(CustomAuthenticationManager customAuthenticationManager) {
+    public SpringConfig(CustomAuthenticationManager customAuthenticationManager, CorsConfigurationSource corsConfigurationSource) {
         this.customAuthenticationManager = customAuthenticationManager;
+        this.corsConfigurationSource = corsConfigurationSource;
     }
 
     @Bean
@@ -29,6 +32,7 @@ public class SpringConfig {
         jwtAuthentication.setFilterProcessesUrl("/api/v1/auth/login");
         
         http
+        .cors(cors -> cors.configurationSource(corsConfigurationSource))
         .csrf(csrf -> csrf.disable())
         .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
         .authorizeHttpRequests(request -> request
