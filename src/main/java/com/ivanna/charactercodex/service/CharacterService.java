15d72d2c2ee59c8dad13909172a19254dfa04604
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-07-27 20:11:14 +0200

    feat: create CharacterService with CRUD

---
+++ src/main/java/com/ivanna/charactercodex/service/CharacterService.java
@@ -0,0 +1,20 @@
+package com.ivanna.charactercodex.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.ivanna.charactercodex.dto.request.CharacterCreateDto;
+import com.ivanna.charactercodex.dto.response.CharacterDetailResponseDto;
+import com.ivanna.charactercodex.dto.response.CharacterListResponseDto;
+
+public interface CharacterService {
+    List<CharacterListResponseDto> getAllCharacters(String userEmail);
+
+    CharacterDetailResponseDto getCharacterById(UUID characterId, String userEmail);
+
+    CharacterDetailResponseDto createCharacter(CharacterCreateDto dto, String userEmail);
+
+    CharacterDetailResponseDto updateCharacter(UUID characterId, CharacterCreateDto dto, String userEmail);
+
+    void deleteCharacter(UUID characterId, String userEmail);
+}
